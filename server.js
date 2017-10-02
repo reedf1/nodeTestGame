e4e4e3883d3c3cb7101ec13ce025b7b3e9f1dfdb
Reed Roberts <reedf1@gmail.com>
2017-10-02 16:55:30 +0100

    Made the game multiplayer

--- server.js
+++ server.js
@@ -14,3 +14,45 @@ app.get('/',function(req,res){
 server.listen(process.env.PORT || 8081,function(){
     console.log('Listening on '+server.address().port);
 });
+
+server.lastPlayderID = 0; 
+
+io.on('connection',function(socket){
+    socket.on('newplayer',function(){
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(100,1600),
+            y: randomInt(100,1200)
+        };
+    socket.emit('allplayers',getAllPlayers());
+
+    socket.broadcast.emit('newplayer',socket.player);
+
+   	socket.emit('playerfocus',socket.player.id);
+
+    socket.on('click',function(data){
+        console.log('click to '+data.x+', '+data.y);
+        socket.player.x = data.x;
+        socket.player.y = data.y;
+        io.emit('move',socket.player);
+    });
+
+     socket.on('disconnect',function(){
+        io.emit('remove',socket.player.id);
+    });
+    });
+})
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
